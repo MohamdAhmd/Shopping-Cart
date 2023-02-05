4c71446e49d143dc7ad9454a4848978977967852
Mohamed Saber <Mosaber455@gmail.com>
2023-02-05 20:57:02 +0200

    Product Details

--- models/products.model.js
+++ models/products.model.js
@@ -27,5 +27,63 @@ exports.getAllProducts = () => {
       })
   })
 }
+//------------------------------------------------------------------------------
+//########################################################################
+//########################################################################
+exports.filterByCategory = (category) => {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(config.DB_URL)
+      .then(() => {
+        return product.find({ category: category })
+      })
+      .then((products) => {
+        mongoose.disconnect()
+        resolve(products)
+      })
+      .catch((err) => {
+        console.log(err)
+        reject(err)
+      })
+  })
+}
+//########################################################################
+//########################################################################
+exports.getProductDtails = (productId) => {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(config.DB_URL)
+      .then(() => {
+        return product.findById(productId)
+      })
+      .then((product) => {
+        mongoose.disconnect()
+        resolve(product)
+      })
+      .catch((err) => {
+        console.log(err)
+        reject(err)
+      })
+  })
+}
+//########################################################################
+//########################################################################
+exports.getOne = () => {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(config.DB_URL)
+      .then(() => {
+        return product.findOne({})
+      })
+      .then((product) => {
+        mongoose.disconnect()
+        resolve(product)
+      })
+      .catch((err) => {
+        console.log(err)
+        reject(err)
+      })
+  })
+}
 
 const product = mongoose.model('Product', productSchema)
